58891aa8b7f35213de0a57f68883345300384dd1
DarkGeekMS <mohamedshawky911@gmail.com>
2021-04-21 17:18:01 +0200

    update .gitignore and run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
- 
 import os
 from app import app
 
 
 if __name__ == "__main__":
+	# application entry point (localhost:5000)
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
