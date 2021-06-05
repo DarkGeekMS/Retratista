c73428cafd5dce2ebe01f67fd960ca44bfb5101e
DarkGeekMS <mohamedshawky911@gmail.com>
2021-06-05 22:20:14 +0200

    add existing files check to weights download script

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -1,15 +1,25 @@
 # download networks weights and seed data
 
 cd src/stylegan_lib
-mkdir models
+
+if [ ! -d models ]; then
+    mkdir models
+fi
+
 cd models
 
-megadl 'https://mega.nz/#!aExSxQAa!K-X4sgm-suacm9LZlnMctQj5vgZHp7f_Ry4KM7GbhfM'
+if [ ! -f stgan2_model.pt ]; then
+    megadl 'https://mega.nz/#!aExSxQAa!K-X4sgm-suacm9LZlnMctQj5vgZHp7f_Ry4KM7GbhfM'
+fi
 
-megadl 'https://mega.nz/#!3ZwSlLTC!vlbC_I_kw2l5bLGwPuXRM5_nerbJ9WPHeDmhU04l9qQ'
+if [ ! -f initial_seed.npy ]; then
+    megadl 'https://mega.nz/#!3ZwSlLTC!vlbC_I_kw2l5bLGwPuXRM5_nerbJ9WPHeDmhU04l9qQ'
+fi
 
 cd ../text_processing/pybert/output/checkpoints/bert
 
-megadl 'https://mega.nz/#!iQ4iBBYA!2k7bWbeZLixzt3c_ry2sHhHBahol7nL8RRBeoGksu9c'
+if [ ! -f pytorch_model.bin ]; then
+    megadl 'https://mega.nz/#!iQ4iBBYA!2k7bWbeZLixzt3c_ry2sHhHBahol7nL8RRBeoGksu9c'
+fi
 
 cd ../../../../../../..
