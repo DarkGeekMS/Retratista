9a98d8367c90f3e3f6989990cb35c716487f6602
RemondaTalaat <remondatalaat21@gmail.com>
2021-07-18 21:49:30 +0200

    update the text weights for deployment

--- scripts/download_weights_gen.sh
+++ scripts/download_weights_gen.sh
@@ -16,10 +16,15 @@ if [ ! -f initial_seed.npy ]; then
     megadl 'https://mega.nz/#!3ZwSlLTC!vlbC_I_kw2l5bLGwPuXRM5_nerbJ9WPHeDmhU04l9qQ'
 fi
 
-cd ../text_processing/pybert/output/checkpoints/bert
+cd ../text_processing/
+
+if [ ! -d chickepoints ]; then
+    mkdir models
+
+cd ../chickepoints/
 
 if [ ! -f pytorch_model.bin ]; then
-    megadl 'https://mega.nz/#!iQ4iBBYA!2k7bWbeZLixzt3c_ry2sHhHBahol7nL8RRBeoGksu9c'
+    megadl 'https://mega.nz/#!CchDlSJD!Oe3QmtiTCRaqqEIqZSXxlcoqycbXKka2mAjxUq3yJGI'
 fi
 
 cd ../../../../../../..
