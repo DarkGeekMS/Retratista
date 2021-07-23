e22cae9b0265a83157e18ea54effdfacf6dbc772
DarkGeekMS <mohamedshawky911@gmail.com>
2021-07-23 19:26:16 +0200

    download demo video on build

---
+++ ui/download_demo.sh
@@ -0,0 +1,9 @@
+# download user interface assets (demo video)
+
+cd public
+
+if [ ! -f demo.mp4 ]; then
+    megadl 'https://mega.nz/#!6QQimBLJ!zEVLk-3dLZcr35L3e3OeBeYqlexmhSRTNniEuIy3qF0'
+fi
+
+cd ..
